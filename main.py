911962cd9bb17a70a0c04c6e5e7ae1d757118bd1
jysndabuio <jysndabu@outlook.com>
2024-01-26 10:41:16 +0100

    Test

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-test update
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    pubyear = db.Column(db.String(50), nullable=False)
+
+@app.route('/add_book', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        user = request.form
+        new_user = Books(title=user['title'], author=user['author'], pubyear=user['pubyear'])
+        db.session.add(new_user)
+        db.session.commit()
+        list = Books.query.all()
+        return render_template('books.html', list=list)
+    return render_template('add_book.html')
+
+@app.route('/books')
+def members():
+    list = Books.query.all()
+    return render_template('books.html', list=list)
+
+def create_db():
+    with app.app_context():
+        db.create_all()
+
+
+if __name__ == '__main__':
+    create_db()
+    app.run()
